3a8cd2570fed095c59d81d120b92898e6bf17da8
thangasblockchain <thangarajm@icloud.com>
2021-03-31 14:58:15 +0530

    zilliqa javascript

---
+++ zilliqa_javascript/zil.js
@@ -0,0 +1,26 @@
+const { BN, Long, bytes, units } = require('@zilliqa-js/util');
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
+const {
+  toBech32Address,
+  getAddressFromPrivateKey,
+} = require('@zilliqa-js/crypto');
+
+const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
+
+// These are set by the core protocol, and may vary per-chain.
+// You can manually pack the bytes according to chain id and msg version.
+// For more information: https://apidocs.zilliqa.com/?shell#getnetworkid
+
+const chainId = 333; // chainId of the developer testnet
+const msgVersion = 1; // current msgVersion
+const VERSION = bytes.pack(chainId, msgVersion);
+
+// Populate the wallet with an account
+const privateKey =
+  '81398035f9dc663d4c14cda1ade5290f9af428e7ab8da14422dd0d31b29b8282';
+
+zilliqa.wallet.addByPrivateKey(privateKey);
+
+const address = getAddressFromPrivateKey(privateKey);
+console.log(`My account address is: ${address}`);
+console.log(`My account bech32 address is: ${toBech32Address(address)}`);
